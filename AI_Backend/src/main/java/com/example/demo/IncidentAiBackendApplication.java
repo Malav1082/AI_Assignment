e4e62520f9b15a34d6650363a0c316896a2f1211
Malav Shah <malavshah1082@gmail.com>
2025-09-13 13:02:45 +0530

    Added AI_Backend

---
+++ AI_Backend/src/main/java/com/example/demo/IncidentAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentAiBackendApplication.class, args);
+	}
+
+}
